44ca93eb401d3a649ad3c99d3b6342d0bc01a845
Max Mattone <maxmattone@maxs-mbp.lan>
2020-12-03 17:40:36 +0100

    first commit

---
+++ test_amazon.py
@@ -0,0 +1,57 @@
+import os
+
+from datetime import datetime
+import pytest
+import json
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+
+now = datetime.now()
+buildName = now.strftime("%m/%d/%Y, %H:%M:%S")
+
+with open('browsers.json') as json_file:
+    browsers = json.load (json_file)
+    
+@pytest.fixture(scope="module", params=browsers)
+def driver(request):
+    username = os.getenv("BROWSERSTACK_USERNAME")
+    access_key = os.getenv("BROWSERSTACK_ACCESS_KEY")
+    caps = browsers[request.param]
+    caps["browserstack.user"] = username
+    caps["browserstack.key"] = access_key
+    caps["project"] = 'CE-Challenge'
+    caps["name"] = "amazon-scroller"
+    caps["build"] = buildName
+    driver = webdriver.Remote(
+        command_executor="https://hub-cloud.browserstack.com/wd/hub",
+        desired_capabilities=caps)
+    yield driver
+    driver.quit()
+
+def test_amazon(driver):
+    driver.get("https://www.amazon.com/")
+    searchInput = driver.find_element_by_class_name("nav-search-field").find_element_by_tag_name("input")
+    searchInput.send_keys("iPhone X")
+    driver.find_element_by_xpath("//input[@value='Go']").click()
+
+    driver.find_element_by_id("low-price").send_keys("0")
+    driver.find_element_by_id("high-price").send_keys("1000")
+    driver.find_element_by_id("high-price").send_keys(Keys.ENTER)
+
+    WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.ID, "filters")))
+    driver.find_element_by_id("filters").find_element_by_xpath("//span[text()='iOS']").click()
+
+    driver.find_element_by_xpath("//span[text()='Sort by:']").click()
+    driver.find_element_by_link_text("Price: High to Low").click()
+
+    
+    items = driver.find_elements_by_xpath("//span[@data-component-type='s-result-item']")
+    for item in items:
+        assert item.find_element_by_tag_name("h2").text
+        
+
+
+
